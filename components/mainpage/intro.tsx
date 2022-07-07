0410ab5c682c9c6fa9785d466dad9ad78de0bf69
Vladislav Sorokin <sorokinvj@gmail.com>
2022-07-07 21:12:08 +0100

    intro margin fix

--- components/mainpage/intro.tsx
+++ components/mainpage/intro.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 export const Intro: React.FC<Props> = ({ page }) => {
   return (
-    <div className="col-span-3 flex flex-col items-center mt-12 mb-20 md:col-span-8 md:grid md:grid-cols-8 md:gap-x-4 md:items-start">
+    <div className="col-span-3 flex flex-col items-center mt-12 mb-20 md:col-span-8 md:grid md:grid-cols-8 md:gap-x-4 md:items-start md:mt-20">
       <div className="mb-10 md:m-0 md:col-span-2">
         <Image
           src={page.data.authorImage}
