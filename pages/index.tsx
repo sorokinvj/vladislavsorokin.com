9fc43ee75f2badba56e165059dbd96ba5c623dfb
Vladislav Sorokin <sorokinvj@gmail.com>
2022-07-07 20:48:19 +0100

    intro

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,34 @@
 import { MainPageMeta } from "components/mainpage/meta";
-import Container from "components/shared/container";
 import { getMainPageContent } from "lib/api";
 import markdownToHtml from "lib/markdownToHtml";
 import Image from "next/image";
 import { MainPage } from "types/mainPage";
 import markdownStyles from "styles/markdown.module.css";
+import cn from "classnames";
 
 type Props = {
   page: MainPage;
 };
 
 const Index: React.FC<Props> = ({ page }) => (
-  <Container className="lg:columns-2 max-w-6xl lg:mt-28">
+  <>
     <MainPageMeta meta={page.data.meta} />
-    <Image
-      src={page.data.authorImage}
-      priority
-      alt="Photo of Vladislav Sorokin"
-      width="600"
-      height="450"
-    />
-    <Container className="p-12 break-before-column">
+    <div className="col-span-3 flex flex-col items-center mt-12 mb-20">
+      <div className="mb-10">
+        <Image
+          src={page.data.authorImage}
+          priority
+          alt="Photo of Vladislav Sorokin"
+          width={156}
+          height={156}
+        />
+      </div>
       <div
-        className={markdownStyles["markdown"]}
+        className={cn(markdownStyles["intro"], "text-center mx-auto")}
         dangerouslySetInnerHTML={{ __html: page.content }}
       />
-    </Container>
-  </Container>
+    </div>
+  </>
 );
 
 export default Index;
