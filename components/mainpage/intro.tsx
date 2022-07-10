7523b548ee5eea23b94a4b6139739495b22be9bc
Vladislav Sorokin <sorokinvj@gmail.com>
2022-07-10 16:16:46 +0100

    new grid class

--- components/mainpage/intro.tsx
+++ components/mainpage/intro.tsx
@@ -10,8 +10,8 @@ interface Props {
 
 export const Intro: React.FC<Props> = ({ page }) => {
   return (
-    <div className="col-span-3 flex flex-col items-center mt-12 mb-20 md:col-span-8 md:grid md:grid-cols-8 md:gap-x-4 md:items-start md:mt-20">
-      <div className="mb-10 md:m-0 md:col-span-2">
+    <div className="col-span-3 grid-container items-center mt-12 mb-20 md:col-span-8 md:items-start md:mt-20">
+      <div className="col-span-3 mx-auto mb-10 md:m-0 md:col-span-2">
         <Image
           src={page.data.authorImage}
           priority
@@ -23,7 +23,7 @@ export const Intro: React.FC<Props> = ({ page }) => {
       <div
         className={cn(
           mainPageStyles["intro"],
-          "text-center mx-auto md:text-left md:col-span-5 md:m-0"
+          "col-span-3 text-center mx-auto md:text-left md:col-span-5 md:m-0"
         )}
         dangerouslySetInnerHTML={{ __html: page.intro }}
       />
--- components/shared/layout/index.tsx
+++ components/shared/layout/index.tsx
@@ -1,4 +1,5 @@
 import { Navigation } from "../navigation";
+import { Footer } from "./footer";
 
 type Props = {
   className?: string;
@@ -8,27 +9,11 @@ type Props = {
 const Layout = ({ className, children }: Props) => {
   return (
     <>
-      <div className="w-full grid grid-cols-3 gap-x-4 p-4 md:max-w-4xl md:w-md:grid-cols-8 md:gap-x-10 md:mr-auto md:ml-auto">
+      <div className="grid-container md:max-w-4xl md:mx-auto">
         <Navigation />
         <main className="col-start-1 col-end-4 md:col-end-9">{children}</main>
       </div>
-      <footer className="w-full bg-neutral-50 p-4 py-20 ">
-        <p className="font-title text-4xl text-darkgrey mb-8">
-          Vladislav Sorokin
-        </p>
-        <p className="text-darkgrey font-sans text-base leading-6">
-          You can find me on{" "}
-          <a href="twitter.com/sorokinvj" rel="noopener noreferrer">
-            Twitter
-          </a>
-          ,{" "}
-          <a href="github.com/sorokinvj" rel="noopener noreferrer">
-            GitHub
-          </a>{" "}
-          or email me at{" "}
-          <a href="mailto:sorokinvj@gmail.com">sorokinvj@gmail.com</a>
-        </p>
-      </footer>
+      <Footer />
     </>
   );
 };
