7726554061fc0245916c4f23180a9ce7fb14c284
Vladislav Sorokin <sorokinvj@gmail.com>
2022-07-13 01:17:51 +0100

    favicon

--- components/shared/layout/index.tsx
+++ components/shared/layout/index.tsx
@@ -1,14 +1,15 @@
 import { Navigation } from "../navigation";
+import { GlobalMeta } from "../navigation/globalMeta";
 import { Footer } from "./footer";
 
 type Props = {
-  className?: string;
   children: React.ReactNode | React.ReactNode[];
 };
 
-const Layout = ({ className, children }: Props) => {
+const Layout = ({ children }: Props) => {
   return (
     <>
+      <GlobalMeta />
       <div className="p-4 md:p-0 md:max-w-4xl md:mx-auto">
         <Navigation />
         <main className="my-18 md:my-20">{children}</main>
