f1cbb79be51a5556c3e41b1bd8973900a18700b0
Vladislav Sorokin <sorokinvj@gmail.com>
2022-09-17 18:36:12 +0100

    install google tag mnager

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,25 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
-
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { GTM_ID } from "../lib/gtm";
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head />
         <body>
+          {GTM_ID && (
+            <noscript>
+              <iframe
+                src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+                height="0"
+                width="0"
+                style={{ display: "none", visibility: "hidden" }}
+              />
+            </noscript>
+          )}
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
