f9f96d68842ad153fac9799363eb58693872d111
Vladislav Sorokin <sorokinvj@gmail.com>
2022-09-17 20:13:44 +0100

    pull out gtm in a component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,9 @@ import "styles/fonts.css";
 import "styles/code.css";
 import Layout from "components/shared/layout";
 import { useEffect } from "react";
-import Script from "next/script";
 import { useRouter } from "next/router";
-import { GTM_ID, pageview } from "../lib/gtm";
+import { pageview } from "lib/gtm";
+import { GoogleTagManager } from "components/shared/gtm/Gtm";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -19,21 +19,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <Layout>
-      {GTM_ID && (
-        <Script
-          id="gtag-base"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer', '${GTM_ID}');
-          `,
-          }}
-        />
-      )}
+      <GoogleTagManager />
       <Component {...pageProps} />
     </Layout>
   );
