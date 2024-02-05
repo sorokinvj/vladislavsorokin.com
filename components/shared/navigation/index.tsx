2a4657774034843fbd2e32a493f1e93c161e7865
Vladislav Sorokin <sorokinvj@gmail.com>
2024-02-05 14:06:35 +0000

    feat: sitemap

--- components/shared/navigation/index.tsx
+++ components/shared/navigation/index.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { Logo } from "./Logo";
-import { NavigationMenu } from "./NavigationMenu";
 
 export const Navigation = () => {
   return (
     <header className="mt-8 md:mt-10">
       <nav className="flex flex-wrap items-center justify-between md:flex-nowrap">
         <Logo />
-        <NavigationMenu />
       </nav>
     </header>
   );
---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteUrl: "https://www.vladislavsorokin.com",
+  generateRobotsTxt: true, // Generate a robots.txt
+  sitemapSize: 7000, // Split sitemap into multiple files if you have more URLs
+  exclude: ["/posts/drafts"],
+};
